a4412b19b1a958a5d262eb18c9f000769d142b21
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-01-16 13:41:14 +0100

    Update

--- main.js
+++ main.js
@@ -9,11 +9,20 @@ document.querySelector("#frontPage").addEventListener("click", (event) => {
     if(event.target.id == "switchToRegister"){
         logIn.classList.add("hide");
         createAccount.classList.remove("hide");
-        console.log("Register Mode");
     }
     else if(event.target.id == "switchToLogIn"){
         createAccount.classList.add("hide");
         logIn.classList.remove("hide");
-        console.log("Log In Mode");
+    };
+});
+
+document.querySelector("#frontPage").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if(event.target.id == "logInButton"){
+        console.log("you are logged in");
+    }
+    else if(event.target.id == "registerButton"){
+        console.log("you have registered in");
     };
 });
