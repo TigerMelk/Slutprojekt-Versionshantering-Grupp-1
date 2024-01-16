363addf6b7f6c304332141b709d646ec184a549a
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-16 14:16:03 +0100

    test fetch temporary

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ document.querySelector("#frontPage").addEventListener("click", (event) => {
     const logIn = document.querySelector("#logIn");
     const createAccount = document.querySelector("#createAccount");
 
-    if(event.target.id == "switchToRegister"){
+    if (event.target.id == "switchToRegister") {
         logIn.classList.add("hide");
         createAccount.classList.remove("hide");
         console.log("Register Mode");
     }
-    else if(event.target.id == "switchToLogIn"){
+    else if (event.target.id == "switchToLogIn") {
         createAccount.classList.add("hide");
         logIn.classList.remove("hide");
         console.log("Log In Mode");
     };
 });
+
+const url = 'https://helloworld-9d8f2-default-rtdb.europe-west1.firebasedatabase.app/';
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
