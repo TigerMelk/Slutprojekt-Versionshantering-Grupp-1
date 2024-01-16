b186208144a88a6955c976773c0e2c5902689479
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-01-16 13:52:12 +0100

    Switch funktionen fixad

--- main.js
+++ main.js
@@ -4,25 +4,24 @@ document.querySelector("#frontPage").addEventListener("click", (event) => {
     event.preventDefault();
 
     const logIn = document.querySelector("#logIn");
+    const logInButton = document.querySelector("#logInButton");
     const createAccount = document.querySelector("#createAccount");
+    const registerButton = document.querySelector("#registerButton");
 
     if(event.target.id == "switchToRegister"){
         logIn.classList.add("hide");
+        logInButton.classList.add("hide");
         createAccount.classList.remove("hide");
+        registerButton.classList.remove("hide");
     }
     else if(event.target.id == "switchToLogIn"){
         createAccount.classList.add("hide");
+        registerButton.classList.add("hide")
         logIn.classList.remove("hide");
+        logInButton.classList.remove("hide");
     };
 });
 
 document.querySelector("#frontPage").addEventListener("submit", (event) => {
     event.preventDefault();
-
-    if(event.target.id == "logInButton"){
-        console.log("you are logged in");
-    }
-    else if(event.target.id == "registerButton"){
-        console.log("you have registered in");
-    };
 });
