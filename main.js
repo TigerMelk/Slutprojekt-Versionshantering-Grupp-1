de2c0654a755e02c7afd234a90ff87b0b961e144
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-16 14:23:41 +0100

    tog bort fetch från main

--- main.js
+++ main.js
@@ -25,10 +25,3 @@ document.querySelector("#frontPage").addEventListener("click", (event) => {
 document.querySelector("#frontPage").addEventListener("submit", (event) => {
     event.preventDefault();
 });
-
-const url = 'https://helloworld-9d8f2-default-rtdb.europe-west1.firebasedatabase.app/';
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
