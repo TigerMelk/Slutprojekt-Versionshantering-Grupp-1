835a8d21c55c58b8cc0d6e9402ee4ec87d2259f1
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-01-16 15:07:13 +0100

    Prettier

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 /*Tests for fetchs */
-const url = 'https://helloworld-9d8f2-default-rtdb.europe-west1.firebasedatabase.app/';
+const url =
+  "https://helloworld-9d8f2-default-rtdb.europe-west1.firebasedatabase.app/";
 fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
