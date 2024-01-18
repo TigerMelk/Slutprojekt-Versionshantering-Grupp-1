61f2c240e96c284bf8e79a4affc580ea6c7c4d50
Abdulla Jamal <93247639+SweepCodes@users.noreply.github.com>
2024-01-18 13:18:38 +0100

    test update

--- test.js
+++ test.js
@@ -1,41 +1,108 @@
 /*Tests for fetch*/
-// async function getFetch(id) {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, onValue, remove, push, onChildAdded, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"; // <---
 
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCf1AUREbV5BzyIb7_5nrj4fjFxcFL8jM8",
+
+  authDomain: "spychat-a5f8e.firebaseapp.com",
+
+  databaseURL: "https://spychat-a5f8e-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "spychat-a5f8e",
+
+  storageBucket: "spychat-a5f8e.appspot.com",
+
+  messagingSenderId: "644385877729",
+
+  appId: "1:644385877729:web:eeab1dc8e5763371a511a0"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const BASE_URL = "https://spychat-a5f8e-default-rtdb.europe-west1.firebasedatabase.app/boys/.json";
+
+
+// function writeUserData(userId, name, email, imageUrl) {
+//   set(ref(db, 'users/' + userId), {
+//     username: name,
+//     email: email,
+//     profile_picture: imageUrl
+//   });
 // }
-// async function postFetch(id) {
 
+// writeUserData("hej", "hello", "tjena", "ok")
+
+
+async function getData() {
+  let response = await fetch(BASE_URL);
+  let data = await response.json();
+  console.log(data);
+}
+getData();
+// async function postData() {
+//   let messageObject = { text: "Hello world", time: new Date() };
+
+//   const requestOptions = {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(messageObject),
+//   };
+
+//   let response = await fetch(BASE_URL, requestOptions);
+//   let data = await response.json();
+//   console.log(data);
 // }
-// async function putFetch(id) {
+async function putData() {
+  let messageObject = { text: "Hello world put", time: new Date() };
 
-// }
-// async function patchFetch(id) {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(messageObject),
+  };
 
-// }
-// async function deleteFetch(id) {
-
-// }
-
-
-
-
-const url =
-  "https://spychat-a5f8e-default-rtdb.europe-west1.firebasedatabase.app/boys/.json";
-fetch(url, {
-  method: "POST",
-  body: JSON.stringify({
-    username: "blahname",
-    password: 1338
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
-
-//unfinsished function
-function userameAndPassword(name, password) {
+  let response = await fetch(BASE_URL, requestOptions);
+  let data = await response.json();
+  console.log(data);
 
 }
+putData();
+// async function patchData() {
+//   let messageObject = { firstName: "Alrik", lastName: "HE" };
+
+//   const requestOptions = {
+//     method: "PATCH",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(messageObject),
+//   };
+
+//   let response = await fetch("https://demo1-3c759-default-rtdb.europe-west1.firebasedatabase.app/-NoCf2s5SInOIFR3X0VX/.json", requestOptions);
+//   let data = await response.json();
+//   console.log(data);
+
+// }
+// async function deleteData() {
+//   const requestOptions = {
+//     method: "DELETE",
+//   };
+//   let response = await fetch(BASE_URL, requestOptions);
+//   let data = await response.json();
+//   console.log(data);
+// }
+
+
+
+
+
+
+
+// //unfinsished function
+// function userameAndPassword(name, password) {
+
+// }
